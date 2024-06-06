e39ee451f0ac7a198cece81d05725b0bfeda4b6e
Kavya S <ksulakhe756@gmail.com>
2024-06-06 13:50:36 +0530

    new commit

---
+++ src/main/java/kavya/demo/kavya_dev/KavyaDevApplication.java
@@ -0,0 +1,13 @@
+package kavya.demo.kavya_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavyaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavyaDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kavya/demo/kavya_dev/KavyaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package kavya.demo.kavya_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavyaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
